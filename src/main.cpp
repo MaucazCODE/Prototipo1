3a56edc6012c43a2a13426ac597a710d0ff89e25
MaucazCODE <114625120+MaucazCODE@users.noreply.github.com>
2025-06-19 18:22:04 -0500

    intento de controlar motores
    Intento fallido, no carga

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,55 @@
 #include <EEPROM.h>
+#include <AccelStepper.h>
+#include "Adafruit_VL53L0X.h"
 
-//const int EEPROM_SIZE = 512; // Define el tamaño de la EEPROM (ajusta según tu necesidad)
 
-// Declaracion de pines
-int negro1 = 12;
-int amarillo1=14;
-int rojo1=27;
-int negro2 = 28;
-int amarillo2=25;
-int rojo2=33;
+int M1_0 = 15;
+int M1_1 = 2;
+int M1_2 = 4;
+int M1_3 = 5;
 
-int tiempo = 2000;
+int M2_0 = 13;
+int M2_1 = 12;
+int M2_2 = 14;
+int M2_3 = 27;
+
+int del = 5;
 
 void setup() {
-  pinMode(negro1,OUTPUT);
-  pinMode(amarillo1,OUTPUT);
-  pinMode(rojo1,OUTPUT);
-  pinMode(negro2,OUTPUT);
-  pinMode(amarillo2,OUTPUT);
-  pinMode(rojo2,OUTPUT);
-} 
+  pinMode(M1_0, OUTPUT);
+  pinMode(M1_1, OUTPUT);
+  pinMode(M1_2, OUTPUT);
+  pinMode(M1_3, OUTPUT);
+}
 
 void loop() {
 
-//vuelta de primera rueda
-  digitalWrite(negro1,HIGH); 
-  delay(tiempo);
-  digitalWrite(amarillo1,HIGH);  
-  delay(tiempo);
-  digitalWrite(rojo1,HIGH);  
-  delay(tiempo);
+  //uno
+  digitalWrite(M1_0, LOW);
+  digitalWrite(M1_1, LOW);
+  digitalWrite(M1_2, LOW);
+  digitalWrite(M1_3, HIGH);
+  delay(del);
 
-//vuelta de 2da rueda
-  digitalWrite(negro2,HIGH); 
-  delay(tiempo);
-  digitalWrite(amarillo2,HIGH);  
-  delay(tiempo);
-  digitalWrite(rojo2,HIGH);
-  delay(tiempo);  
+  //dos
+  digitalWrite(M1_0, LOW);
+  digitalWrite(M1_1, LOW);
+  digitalWrite(M1_2, HIGH);
+  digitalWrite(M1_3, LOW);
+  delay(del);
+
+  //tres
+  digitalWrite(M1_0, LOW);
+  digitalWrite(M1_1, HIGH);
+  digitalWrite(M1_2, LOW);
+  digitalWrite(M1_3, LOW);
+  delay(del);
+
+  //cuarto
+  digitalWrite(M1_0, HIGH);
+  digitalWrite(M1_1, LOW);
+  digitalWrite(M1_2, LOW);
+  digitalWrite(M1_3, LOW);
+  delay(del);
+
 }
