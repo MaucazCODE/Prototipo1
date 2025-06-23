3307ba37eb64b83519b30acd1528387ec0abc293
Trejo-Miguel <106838582+Trejo-Miguel@users.noreply.github.com>
2025-06-22 19:54:49 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,52 +20,31 @@ Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 
 void setup() {
+  Serial.begin(115200);
+  Wire.begin(21, 22);  // Asegúrate de usar los pines correctos
 
-
-  pinMode(M1_0, OUTPUT);
-  pinMode(M1_1, OUTPUT);
-  pinMode(M1_2, OUTPUT);
-  pinMode(M1_3, OUTPUT);
-
-  /*Yo hice esta parte Motor 2*/
-  pinMode(M2_0, OUTPUT);
-  pinMode(M2_1, OUTPUT);
-  pinMode(M2_2, OUTPUT);
-  pinMode(M2_3, OUTPUT);
-
-Serial.begin(115200);
-
-  // wait until serial port opens for native USB devices
-  while (! Serial) {
-    delay(1);
-  }
- 
-  Serial.println("Adafruit VL53L0X test");
   if (!lox.begin()) {
-    Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
-    }
+    Serial.println("Fallo al encontrar VL53L0X");
+    while (1);
+  }
 
-// power 
-  Serial.println(F("VL53L0X API Simple Ranging example\n\n"));
+  Serial.println("Sensor VL53L0X iniciado correctamente");
 
 }
 
 void loop() {
-  
-  
   VL53L0X_RangingMeasurementData_t measure;
-   
-  Serial.print("Reading a measurement... ");
-  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+  lox.rangingTest(&measure, false); // true = debug!
 
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  if (measure.RangeStatus != 4) {  // 4 = fuera de rango
+    Serial.print("Distancia (mm): "); Serial.println(measure.RangeMilliMeter);
   } else {
-    Serial.println(" out of range ");
+    Serial.println("Fuera de rango");
   }
-   
-  delay(100);
+
+  delay(1000);
+  
+  
   
   
   
